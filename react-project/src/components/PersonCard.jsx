fcba17b1d485b75e9788277068ae3b8444cb1788
khayatcode <a.khayatcode0103@gmail.com>
2023-04-01 17:00:01 -0600

    second commit

--- react-project/src/components/PersonCard.jsx
+++ react-project/src/components/PersonCard.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    increaseAge = () => {
+        this.setState({ age: this.state.age + 1 });
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.increaseAge }>Increase Age</button>
             </div>
         )
     }
